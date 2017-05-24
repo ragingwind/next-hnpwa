952ed901ca1bcb3271c17fd0d4e16a8c4fc39f96
ragingwind <ragingwind@gmail.com>
2017-05-24 09:34:11 +0900

    feat: add favicon

--- components/page.js
+++ components/page.js
@@ -8,6 +8,7 @@ export default ({children}) => (
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <meta name="theme-color" content="#000000" />
+      <link rel="icon" href="/static/favicon.ico" />
       <title>Hacker News with Next.js</title>
     </Head>
     <Nav />
