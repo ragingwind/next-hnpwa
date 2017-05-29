665c7c0f915a52ed012b4376bd137ee3901c4061
ragingwind <ragingwind@gmail.com>
2017-05-30 08:23:11 +0900

    fix: Update profile ui

--- components/user.js
+++ components/user.js
@@ -1,17 +1,13 @@
 export default({user}) => (
   <div>
-    <h3>{user.id}</h3>
     <ul>
+      <h1>{user.id}</h1>
       <li>Created: {user.created}</li>
       <li>Karma: {user.karma}</li>
       <li>Delay: {user.delay}</li>
       <li>About: {user.about}</li>
     </ul>
     <style jsx>{`
-      ul {
-        padding: 0
-      }
-
       li {
         margin: 10px, 0;
         list-style-type: none;
