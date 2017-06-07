2232fc517351ea5e1aa325e51e85c33129384662
ragingwind <ragingwind@gmail.com>
2017-06-07 14:45:54 +0900

    build: Bumup firebase-hackernews

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   webpack: (config) => {
-    // prevent build error
-    config.plugins = config.plugins.filter((plugin) => {
-      if (plugin.constructor.name === 'UglifyJsPlugin') {
-        return false
-      } else {
-        return true
-      }
-    })
-
     return config
   }
 }
