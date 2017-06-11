4223c83074323cf4f7650c5b8f4baca90e48e3f1
ragingwind <ragingwind@gmail.com>
2017-06-11 14:45:41 +0900

    build: Add report build with webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 
 module.exports = {
   webpack: async (config, { dev }) => {
@@ -12,6 +13,17 @@ module.exports = {
       "react": "preact-compat/dist/preact-compat",
       "react-dom": "preact-compat/dist/preact-compat",
       "firebase-hackernews": "firebase-hackernews/es"
+    }
+
+    // yarn build:report
+    if (process.env.npm_config_report) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'disabled',
+          generateStatsFile: true,
+          statsFilename: 'stats.json'
+        })
+      )
     }
 
     return config
