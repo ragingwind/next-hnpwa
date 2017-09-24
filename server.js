46d1273afc9015e54b0db6852854c184de5d0813
Jimmy Moon <ragingwind@gmail.com>
2017-09-24 19:36:41 +0200

    fix: optimze server configuration

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const next = require('next')
 const Router = require('router')
 const finalhandler = require('finalhandler')
 const firebase = require('firebase')
-const compression = require('compression')
 const hackernews = require('firebase-hackernews')
 
 // instance for services
@@ -16,50 +15,50 @@ const hnservice = hackernews.init(firebase)
 
 // serve functions
 const page = async (req, res) => {
-  req.url = req.originalUrl
-  micro.send(res, 200, await handle(req, res))
+	req.url = req.originalUrl
+	micro.send(res, 200, await handle(req, res))
 }
 const stories = async (req, res) => {
-  micro.send(res, 200, await hnservice.fetch(req.originalUrl))
+	micro.send(res, 200, await hnservice.fetch(req.originalUrl))
 }
 const static = (req, res, dest) => {
-  return new Promise((resolve, reject) => {
-    if (!dest) {
-      dest = `.${req.originalUrl}`
-    }
+	return new Promise((resolve, reject) => {
+		if (!dest || typeof dest !== 'string') {
+			dest = `.${req.originalUrl}`
+		}
 
-    fs.readFile(dest, (err, data) => {
-      if (err) {
-        reject(err)
-        return
-      }
+		fs.readFile(dest, (err, data) => {
+			if (err) {
+				reject(err)
+				return
+			}
 
-      if (/\.js$/.test(dest)) {
-        res.setHeader('Content-Type', 'application/javascript')
-      } else if (/\.json$/.test(dest)) {
-        res.setHeader('Content-Type', 'application/json')
-      }
+			if (/\.js$/.test(dest)) {
+				res.setHeader('Content-Type', 'application/javascript')
+			} else if (/\.json$/.test(dest)) {
+				res.setHeader('Content-Type', 'application/json')
+			}
 
-      micro.send(res, 200, data)
+			micro.send(res, 200, data)
 
-      resolve()
-    })
-  })
+			resolve()
+		})
+	})
 }
 
 app.prepare()
 	.then(() => hnservice.watch())
 	.then(() => {
-    const router = new Router()
+		const router = new Router()
 
-    router.use(compression({threshold: 0}))
-    router.get('/', page)
-    router.use('/_next', page)
-    router.use('/static', static)
-    router.get('/service-worker.js', async (req, res) => await static(req, res, `./.next/service-worker.js`))
-    router.get('/manifest.jso', async (req, res) => await static(req, res, `./static/manifest.jso`))
-    router.use('/hackernews/:story', stories)
+		router.get('/service-worker.js', async (req, res) => await static(req, res, `./.next/service-worker.js`))
+		router.get('/manifest.json', async (req, res) => await static(req, res, `./static/manifest.json`))
+		router.get('/favicon.ico', async (req, res) => await static(req, res, `./static/favicon.ico`))
+		router.use('/hackernews/:story', stories)
+		router.use('/_next', page)
+		router.use('/static', static)
+		router.get('/', page)
 
-    const server = micro((req, res) => router(req, res, finalhandler(req, res)))
-    server.listen(3000)
-  })
+		const server = micro((req, res) => router(req, res, finalhandler(req, res)))
+		server.listen(3000)
+	})
